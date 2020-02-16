50a140e6476b62c1145068ddcaff9474cc9cf0a7
iPhone-a <ai.tomato.1123@gmail.com>
2020-02-16 21:02:38 +0900

    update

--- index.py
+++ index.py
@@ -4,4 +4,4 @@ from bottle import route, run, template
 def index():
   return 'Bottle sample'
 
-run(host='localhost', port=8080, debug=True, reloader=True)
+run(host='localhost', port=8080, debug=True)
